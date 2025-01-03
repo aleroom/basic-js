271ee1a2cb0ee2a4380327e30d1143faf81f8dc2
aleroom <ale.cherniavskaya@gmail.com>
2025-01-03 19:19:27 +0300

    feat: add some tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity <= 0 || isNaN(sampleActivity)) {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if (activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+  const age = (HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / activity)) / Math.log(2);
+
+  return Math.ceil(age);
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const strNum = n.toString();
+  let maxNum = 0;
+  for (let i = 0; i < strNum.length; i++) {
+    const newNum = parseInt(strNum.slice(0, i) + strNum.slice(i + 1), 10);
+    if (newNum > maxNum) {
+      maxNum = newNum;
+    }
+  }
+  return maxNum;
 }
 
 module.exports = {
